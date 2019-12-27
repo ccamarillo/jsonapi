ca29d0aa72de4e026985745666cd1cc267c3b7de
Candace Camarillo <candacec@vacasa.com>
2019-12-27 11:36:13 -0800

    remove cruft

--- app/__init__.py
+++ app/__init__.py
@@ -3,14 +3,12 @@ from flask_sqlalchemy import SQLAlchemy
 from gevent import monkey
 from flask_rest_jsonapi import Api
 import logging
-from appoptics_apm.middleware import AppOpticsApmMiddleware
 
 import json
 
 logging.basicConfig(level=logging.DEBUG)
 
 app = Flask(__name__)
-app.wsgi_app = AppOpticsApmMiddleware(app.wsgi_app)
 db = SQLAlchemy(app)
 api = Api(app)
 monkey.patch_all()  # This prevents a threading error
