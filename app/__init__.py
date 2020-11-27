e9598dbb6076ded101441e21a2cf44f23916fe88
Candace Camarillo <candace.fox.camarillo@gmail.com>
2020-11-27 15:11:20 -0800

    enable CORS

--- app/__init__.py
+++ app/__init__.py
@@ -1,4 +1,5 @@
 from flask import Flask
+from flask_cors import CORS
 from flask_sqlalchemy import SQLAlchemy
 from flask_rest_jsonapi import Api
 import logging
@@ -13,6 +14,7 @@ app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 app.config['SQLALCHEMY_DATABASE_URI'] = os.environ.get('SQLALCHEMY_DATABASE_URI');
 db = SQLAlchemy(app)
 api = Api(app)
+CORS(app)
 
 db.init_app(app)
 
